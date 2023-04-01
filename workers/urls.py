928666d213d2fce08838134d5df75245d17c35a5
Aiven Timptner <aiven@timptner.de>
2023-04-01 20:12:19 +0200

    Disable registration

--- workers/urls.py
+++ workers/urls.py
@@ -5,7 +5,7 @@ from workers import views
 app_name = 'workers'
 urlpatterns = [
     path('', views.LandingPage.as_view(), name='landing_page'),
-    path('bewerben/', views.ApplicationFormView.as_view(), name='apply'),
+    # path('bewerben/', views.ApplicationFormView.as_view(), name='apply'),
     path('fertig/', views.SuccessView.as_view(), name='success'),
     path('impressum/', views.SiteNoticeView.as_view(), name='site_notice'),
     path('datenschutz/', lambda request: redirect('https://stura-md.de/datenschutz/'), name='privacy_policy'),
